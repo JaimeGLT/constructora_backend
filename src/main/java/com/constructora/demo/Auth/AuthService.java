b38d5533e7aa3a6a74098f1a754100ce0149cb24
JaimeGLT <jaimeguzmanlt@gmail.com>
2026-02-13 08:40:44 -0400

    sdaf

--- src/main/java/com/constructora/demo/Auth/AuthService.java
+++ src/main/java/com/constructora/demo/Auth/AuthService.java
@@ -1,6 +1,13 @@
 package com.constructora.demo.Auth;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 import org.springframework.stereotype.Service;
+
+import com.constructora.demo.User.Role;
+import com.constructora.demo.User.User;
+import com.constructora.demo.User.UserRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -8,14 +15,27 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AuthService {
 
+    private final UserRepository userRepository;
+
     public AuthResponse login(LoginRequest request) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'login'");
     }
 
     public AuthResponse register(RegisterRequest request) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'register'");
-    }
+        User user = User.builder()
+                .dni(request.getDni())
+                .nombre_completo(request.getNombre_completo())
+                .password_hash(request.getPassword_hash())
+                .rol(Role.USER)
+                .cargo(request.getCargo())
+                .estado(request.getEstado())
+                .created_at(request.getCreated_at())
+                .salario_diario(request.getSalario_diario())
+                .ultimo_gps_texto(request.getUltimo_gps_texto())
+                .build();
 
+        userRepository.save(user);
+
+    }
 }
