50d3b451ba0bc3c4031a76cb4e1a4e13e815b33a
Jaime Guzman <jaimeguzmanlt@gmail.com>
2026-02-26 18:07:55 -0400

    sin permisos register

--- src/main/java/com/constructora/demo/Auth/AuthController.java
+++ src/main/java/com/constructora/demo/Auth/AuthController.java
@@ -2,6 +2,8 @@ package com.constructora.demo.Auth;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,8 +23,13 @@ public class AuthController {
         return ResponseEntity.ok(authService.login(request));
     }
 
+    @GetMapping("test")
+    public ResponseEntity<?> test(Authentication auth) {
+        return ResponseEntity.ok(auth.getAuthorities());
+    }
+
     @PostMapping(value = "register")
-    @PreAuthorize("hasRole('ADMIN')")
+    // @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request) {
         return ResponseEntity.ok(authService.register(request));
     }
--- src/main/java/com/constructora/demo/config/SecurityConfig.java
+++ src/main/java/com/constructora/demo/config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
                                 .disable())
                                 .authorizeHttpRequests(
                                                 authRequest -> authRequest
-                                                                .requestMatchers("/api/login",
+                                                                .requestMatchers("/api/login", "/api/register",
                                                                                 "/swagger-ui/**", "/v3/api-docs/**",
                                                                                 "/swagger-ui.html", "/v3/api-docs.yaml")
                                                                 .permitAll()
